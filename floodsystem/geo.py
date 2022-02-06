c1419e519ec9a12812909ba0cc5f6d221d86b641
Oisheeee <98272978+Oisheeee@users.noreply.github.com>
2022-02-06 17:16:34 +0000

    added geo.py for 1c

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,10 +10,12 @@ from .utils import sorted_by_key  # noqa
 
 def stations_within_radius(stations, centre, r):
     stationslist = stations_by_distance(stations, centre)
-    counter = 0 
-    while stationslist[counter][1] < r:
-        stations.append(stationslist[counter][0])
-        counter = counter +1
+    
+    stations = []
+    for station in stationslist:
+        if station[1] <= r:
+            stations.append(station[0])
+            
     return stations
 
 
