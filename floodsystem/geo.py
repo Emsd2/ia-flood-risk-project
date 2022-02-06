3c550dd1323200e54bb10f5903aa0afb21549f17
Ibshar Oishi <ijo24@cam.ac.uk>
2022-02-06 13:04:53 +0000

    Enter

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,13 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+
+def stations_within_radius(stations, centre, r):
+    stationslist = stations_by_distance(stations, centre)
+    counter = 0 
+    while stationslist[:counter][1] < r: 
+        stations.append(stationslist[counter][0])
+    return stations
+
+
