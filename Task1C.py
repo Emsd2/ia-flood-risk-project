96eca80fc81f9674e18c13f91945d6f4090ee460
Oisheeee <98272978+Oisheeee@users.noreply.github.com>
2022-02-06 14:38:47 +0000

    added 1C and 1F

---
+++ Task1C.py
@@ -0,0 +1,20 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+def run():
+    #list of stations
+    stations = build_station_list()
+    #finding the stations that are within required radius of 10km of the Cambridge City centre
+    stations_required = stations_within_radius(stations, (52.2053, 0.1218), 10)
+    #sorting the stations in alphabetical order
+    station_list = []
+    for station in stations_required:
+        #use a list to sort the stations
+        station_list.append(station.name)
+    print(sorted(station_list))
+    
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
+
+    
