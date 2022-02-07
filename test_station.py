727bb57b93d28b753f9b14b0e42d2d493bad9cf3
Ibshar Oishi <ijo24@cam.ac.uk>
2022-02-07 19:46:30 +0000

    added testing for 1F

--- test_station.py
+++ test_station.py
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
+import pytest
 from floodsystem.station import MonitoringStation
+from floodsystem.station import inconsistent_typical_range_stations
 
 
 def test_create_monitoring_station():
@@ -25,3 +27,28 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_inconsistent_typical_range_stations():
+
+    station_1 = MonitoringStation("S1", "Measure S1", "Station1", (10.0, 40.0), None, "Medium River", "Town1")
+    station_2 = MonitoringStation("S2", "Measure S1", "Station2", (0.0, 10.0), (1.0, 2.0), "Big River", "Town2")
+    station_3 = MonitoringStation("S3", "Measure S3", "Station3", (0.0, 20.0), (2.0, 1.0), "Big River", "Town3")
+
+    station_list = [station_1, station_2, station_3]
+
+    assert type(inconsistent_typical_range_stations(station_list)) == list
+
+    assert len(inconsistent_typical_range_stations(station_list)) == 2
+
+    assert inconsistent_typical_range_stations(station_list)[0] == "Station1"
+
+    assert inconsistent_typical_range_stations(station_list)[1] == "Station3"
+
+
+
+
+
+
+    
+
+
