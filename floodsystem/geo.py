15ad1f1da0fcffe485449bcf33fbe1e560afb9ca
Emmanuelle Dumont <emsd2@cam.ac.uk>
2022-02-08 12:01:43 +0000

    Edited the sort_by_key

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -32,9 +32,9 @@ def stations_by_distance(stations, p):
         temp = (station.name, distance)
         stations_list.append(temp)
 
-    sorted_by_key(stations_list, 1)
+    stations_list = sorted_by_key(stations_list, 1)
     #stations_list.sort(key = lambda a:a[1])
-    return  stations_list
+    return stations_list
 
 
 def rivers_with_station(stations):
@@ -77,7 +77,7 @@ def rivers_by_station_number(stations, N):
         number_of_stations = len(stations_river[river])
         number_list.append((river, number_of_stations))
     #To sort a list of tuples by the second element of the tuple and so that the list is in descending order
-    sorted_by_key(number_list, 1, reverse = True)
+    number_list = sorted_by_key(number_list, 1, reverse = True)
     #number_list.sort(key = lambda a:a[1], reverse = True)
 
     counter = 0
